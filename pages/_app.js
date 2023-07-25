faa942564c180c815679fea52f5a342a7cecb807
Piyush Sharma <piyush.sharma.code@gmail.com>
2023-07-26 00:58:29 +0530

    Co-authored-by: Akash Gandhar <akashgandhar@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
-import useNeonCursor from "@/hooks/useNeonCursor";
 import "@/styles/globals.css";
 
 
 export default function App({ Component, pageProps }) {
-  useNeonCursor();
+ 
   return <Component {...pageProps} />;
 }
