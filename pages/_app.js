98f1cc4ab3bb8f705722554ad031fafeee22efac
school-management-868 <smanagement868@gmail.com>
2023-07-26 10:45:03 +0530

    dfs

--- pages/_app.js
+++ pages/_app.js
@@ -15,26 +15,27 @@ export default function App({ Component, pageProps }) {
     <>
       {isBrowser && (
         <div className="App">
-          <AnimatedCursor 
-          innerSize={12}
-          outerSize={35}
-          innerScale={1}
-          outerScale={2}
-          // outerAlpha={0}
-          hasBlendMode={true}
-          clickables={[
-            'a',
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            'label[for]',
-            'select',
-            'textarea',
-            'button',
-            '.link'
-          ]}/>
+          <AnimatedCursor
+            innerSize={12}
+            outerSize={35}
+            innerScale={1}
+            outerScale={2}
+            // outerAlpha={0}
+            hasBlendMode={true}
+            clickables={[
+              "a",
+              'input[type="text"]',
+              'input[type="email"]',
+              'input[type="number"]',
+              'input[type="submit"]',
+              'input[type="image"]',
+              "label[for]",
+              "select",
+              "textarea",
+              "button",
+              ".link",
+            ]}
+          />
         </div>
       )}
       <Component {...pageProps} />
