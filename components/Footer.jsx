acc02279ef99a8dda0ef3568fa77f6dfd0facc23
PiyushSharma0 <piyush.sharma.code@gmail.com>
2023-08-08 14:22:04 +0530

    Final

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,11 @@
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import TelegramIcon from '@mui/icons-material/Telegram';
-import YouTubeIcon from '@mui/icons-material/YouTube';
+import {
+  AiFillYoutube,
+  AiOutlineTwitter,
+  AiFillMediumSquare,
+  AiFillGithub,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { BsDiscord, BsTelegram } from "react-icons/bs";
 
 import Link from "next/link";
 
@@ -11,28 +14,56 @@ const Footer = () => {
     <footer class="w-full  flex items-center justify-center ">
       <div class="md:w-2/3 w-full px-4 text-white flex flex-col">
         <div class="flex flex-col">
-          <div class="flex mt-24 mb-12 flex-row justify-between">
+          <div class="flex mt-24 mb-12 flex-row items-center justify-center xl:justify-between">
             <div class="">
-              <img src="logo.png" alt="logo" class="w-40 object-contain" />
+              <img src="logo.png" alt="logo" class="w-40 object-contain hidden xl:block" />
             </div>
-           <Link href="#about" class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
+            <Link
+              href="#about"
+              class="hidden xl:block cursor-pointer text-gray-600 hover:text-white uppercase"
+            >
               About
-           </Link>
-           <Link href="#work" class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
+            </Link>
+            <Link
+              href="#work"
+              class="hidden xl:block cursor-pointer text-gray-600 hover:text-white uppercase"
+            >
               Products
-           </Link>
-           <Link href="#contact" class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
+            </Link>
+            <Link
+              href="#contact"
+              class="hidden xl:block cursor-pointer text-gray-600 hover:text-white uppercase"
+            >
               Tokenomics
-           </Link>
-           <Link href="#team" class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
+            </Link>
+            <Link
+              href="#team"
+              class="hidden xl:block cursor-pointer text-gray-600 hover:text-white uppercase"
+            >
               Team
-           </Link>
-            <div class="flex flex-row space-x-8 items-center justify-between">
-              <TwitterIcon />
-              <LinkedInIcon />
-              <GitHubIcon />
-              <TelegramIcon />
-              <YouTubeIcon />
+            </Link>
+            <div class="flex flex-row space-x-8 items-center justify-between w-full mx-10 xl:w-auto">
+              <Link href="https://twitter.com/saaarplabs" target="1">
+                <AiOutlineTwitter />
+              </Link>
+              <Link href="https://github.com/saaarplabs" target="1" >
+                <AiFillGithub />
+              </Link>
+              <Link href="https://www.linkedin.com/company/saaarp" target="1">
+                <AiFillLinkedin />
+              </Link>
+              <Link href="https://medium.com/@saaarplabs" target="1">
+                <AiFillMediumSquare />
+              </Link>
+              <Link href="https://www.youtube.com/@saaarp" target="1">
+                <AiFillYoutube />
+              </Link>
+              <Link href="https://discord.com/invite/ZZxXEX9xWG" target="1">
+                <BsDiscord />
+              </Link>
+              <Link href="https://t.me/saaarplabs" target="1">
+                <BsTelegram />
+              </Link>
             </div>
           </div>
           <hr class="border-white" />
